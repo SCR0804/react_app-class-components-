6b548468cbdadf54b5dd456dd3829f5c98a67274
Sergey Yakimovich <yakimovichsergei@gmail.com>
2023-03-13 21:45:29 +0300

    modified files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import AppInfo from './components/AppInfo/AppInfo';
+import SearchPanel from './components/SearchPanel/SearchPanel';
+import AppFilter from './components/AppFilter/AppFilter';
+import EmployeesList from './components/EmployeesList/EmployeesList';
+import EmployeesAddForm from './components/EmployeesAddForm/EmployeesAddForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          name: 'Иванов Иван',
+          salary: 800,
+          increase: true,
+          rise: false,
+          id: 1,
+        },
+        {
+          name: 'Петров Петр',
+          salary: 2500,
+          increase: false,
+          rise: true,
+          id: 2,
+        },
+        {
+          name: 'Сергеев Сергей',
+          salary: 3700,
+          increase: false,
+          rise: false,
+          id: 3,
+        },
+        {
+          name: 'Яшин Яков',
+          salary: 4200,
+          increase: false,
+          rise: false,
+          id: 4,
+        },
+      ],
+      term: '',
+      filter: 'all',
+    };
+    this.maxId = 5;
+  }
+
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter((elem) => elem.id !== id),
+      };
+    });
+  };
+
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      rise: false,
+      id: this.maxId++,
+    };
+    this.setState(({ data }) => {
+      const newArr = [...data, newItem];
+      return {
+        data: newArr,
+      };
+    });
+  };
+
+  onToggleProp = (id, prop) => {
+    this.setState(({ data }) => ({
+      data: data.map((item) => {
+        if (item.id === id) {
+          return { ...item, [prop]: !item[prop] };
+        }
+        return item;
+      }),
+    }));
+  };
+
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+    return items.filter((item) => item.name.includes(term));
+  };
+
+  onUpdateSearch = (term) => {
+    this.setState({ term });
+  };
+
+  filterPost = (items, filter) => {
+    switch (filter) {
+      case 'rise':
+        return items.filter((item) => item.rise);
+      case 'moreThen1000':
+        return items.filter((item) => item.salary > 1000);
+      default:
+        return items;
+    }
+  };
+
+  onFilterSelect = (filter) => {
+    this.setState({ filter });
+  };
+
+  render() {
+    const { data, term, filter } = this.state;
+    const employees = this.state.data.length;
+    const increased = this.state.data.filter((item) => item.increase).length;
+    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
+    return (
+      <div className='app'>
+        <AppInfo employees={employees} increased={increased} />
+
+        <div className='search-panel'>
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
+          <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
+        </div>
+        <EmployeesList
+          data={visibleData}
+          onDelete={this.deleteItem}
+          onToggleProp={this.onToggleProp}
+        />
+        <EmployeesAddForm onAdd={this.addItem} />
+      </div>
+    );
+  }
 }
 
 export default App;
